1efe3a6bc624bae066fff60e66586ee80b96d26e
zhangwuji <191933284@qq.com>
2018-07-26 17:11:09 +0800

    err

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const Koa = require('koa')
 const app = new Koa()
-const { htmlTpl,ejsTpl,pugTpl } = require('./tpl')
-const ejs = require('ejs')
-const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
+
+//使用views .pug的文件作为渲染模板
+app.use(views(resolve(__dirname,'./views'),{
+	extension: 'pug'
+}))
 
 app.use( async(ctx) => {
-	ctx.type = 'text/html;charset=utf-8'
-    ctx.body = pug.render(pugTpl,{
-    	you:'Luke',
-    	me:'Scott'
-    })
+	await ctx.render('index',{
+		you:'Luke',
+		me:'Scott'
+	})
 })
 app.listen(8888)
 console.log("demo in run")
